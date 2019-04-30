d3cb8d90a34d70543631edbf43602819ee545176
Omar Moghazy <omar.moghazy.42@gmail.com>
2019-05-01 00:23:58 +0200

    making it even readier for u ashraf

--- src/app/StoryTwoGui.java
+++ src/app/StoryTwoGui.java
@@ -3,15 +3,17 @@ package app;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
 public class StoryTwoGui {
 	
 	public void start(Stage arg0){
 		Group root = new Group();
-		Canvas canvas = new Canvas();
+		Canvas canvas = new Canvas(1280,720); 
+		GraphicsContext gc = canvas.getGraphicsContext2D();
 		root.getChildren().add(canvas);
-		Scene scene = new Scene(root,650,500);
+		Scene scene = new Scene(root,1280,720);
 		arg0.setScene(scene);
 		arg0.setTitle("Story Two");
 		arg0.show();
